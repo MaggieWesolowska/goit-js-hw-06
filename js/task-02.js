d733f4f3fab300d05c863fd977db25bbaed8c983
MaggieWesolowska <maggiewes07@gmail.com>
2023-02-10 16:20:24 +0100

    edited script js

--- js/task-02.js
+++ js/task-02.js
@@ -7,14 +7,28 @@ const ingredients = [
   'Condiments',
 ];
 
-const ingredientList = document.querySelector("#ingredients");
+// const ingredientList = document.querySelector("#ingredients");
 
-const addItems = ingredients
-  .map((ingredients) => `<li class="item">${ingredients}</li>`)
-  .join(" ");
+// const addItems = ingredients
+//   .map((ingredients) => `<li class="item">${ingredients}</li>`)
+//   .join(" ");
 
-  console.log(addItems);
+//   console.log(addItems);
 
-  ingredientList.innerHTML = addItems;
-  console.log(ingredientList);
+//   ingredientList.innerHTML = addItems;
+//   console.log(ingredientList);
+
+const ingredientList = [];
+
+for (const ingredient of ingredients) {
+  const liItem = document.createElement("li");
+  liItem.textContent = ingredient;
+  liItem.classList.add('item');
+  ingredientList.push(liItem);
+}
+
+const ulList = document.querySelector("#ingredients");
+
+ulList.append(...ingredientList);
+console.log(ulList);
 
