b3dd8181c8c142f9ae6a7deec7ea50e45ecf68b0
MaggieWesolowska <maggiewes07@gmail.com>
2023-02-10 15:52:08 +0100

    edited 02-task js script

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientList = document.querySelector("#ingredients");
+
+const addItems = ingredients
+  .map((ingredients) => `<li class="item">${ingredients}</li>`)
+  .join(" ");
+
+  console.log(addItems);
+
+  ingredientList.innerHTML = addItems;
+  console.log(ingredientList);
+
