698bad801896fec16a8eeab77ed231121377ee93
MaggieWesolowska <maggiewes07@gmail.com>
2023-02-17 14:30:00 +0100

    edited task-09 js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const bodyColor = document.qetElementByTagName(body);
+const randomColor = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+button.addEventListener('click', (getRandomHexColor) => {
+  bodyColor === randomColor;
+})
+
