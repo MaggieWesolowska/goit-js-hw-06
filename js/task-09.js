6927c09773f23f9d39b5cdd44e6239587c2d6a76
MaggieWesolowska <maggiewes07@gmail.com>
2023-02-17 15:18:08 +0100

    corrected task-09 js

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,13 @@
-const bodyColor = document.qetElementByTagName(body);
-const randomColor = document.querySelector('.color');
+const bodyColor = document.body; // or: document.querySelector('body');
+const spanColor = document.querySelector('.color');
 const button = document.querySelector('.change-color');
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-button.addEventListener('click', (getRandomHexColor) => {
-  bodyColor === randomColor;
-})
-
+button.addEventListener('click', () => {
+  const randomHexColor = getRandomHexColor();
+  spanColor.textContent = randomHexColor;
+  bodyColor.style.backgroundColor = randomHexColor;
+});
