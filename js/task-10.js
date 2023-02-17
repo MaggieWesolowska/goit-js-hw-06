25cb1b434a31e8981df9f9af6638f641b159d501
MaggieWesolowska <maggiewes07@gmail.com>
2023-02-17 17:45:40 +0100

    added edits to js

--- js/task-10.js
+++ js/task-10.js
@@ -4,16 +4,15 @@ function getRandomHexColor() {
 const inputSource = document.querySelector('input');
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
-const boxesDiv = document.querySelector('#boxes');
+const divBoxes = document.querySelector('#boxes');
 
 let amount = 0;
 
 function createBoxes(amount) {
   for (i = 0; i < inputSource; i++) {
     const newBox = `<div></div>`;
-    boxesDiv.insertAdjacentHTML(afterbegin, newBox)
+    divBoxes.insertAdjacentHTML(afterbegin, newBox)
     amount += newBox;
-    newBox.style.fontSize = "30 + 10*i + 'px'";
   }
 }
 
@@ -21,5 +20,6 @@ createBtn.addEventListener('click', () => {
   const randomHexColor = getRandomHexColor();
   const addBox = createBoxes();
   addBox.style.backgroundColor = randomHexColor;
+  addBox.style.fontSize = 30 + 10*i + 'px';
 })
 
